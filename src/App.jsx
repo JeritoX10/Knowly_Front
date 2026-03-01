954816975d75008fbedb807ad7c942b5a4e806ba
sharon <sharonasprilla1724@gmail.com>
2026-03-01 15:48:46 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
 import './App.css'
 
 function App() {
@@ -6,8 +7,17 @@ function App() {
 		<div>
 			<NavBar />
 			<main style={{ marginTop: '80px', minHeight: 'calc(100vh - 80px)' }}>
-				{/* Aquí van las páginas */}
+				{/* Aquí van las páginas
+					redes
+					login
+					certificado
+					cursos
+					contacto
+				*/}
 			</main>
+			
+			<Footer />
+
 		</div>
 	)
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,19 @@
 const Footer =()=>{
     return(
         <footer className='footer'>
-            
+            <p> 
+                knowly| Todos los derechos reservados
+
+            </p>
+
+            <p>
+                programadores (5)
+            </p>
+                <p>
+                    <a href="#facebook">Facebook</a>
+                    <a href="#instagram">Instagram</a> 
+                    <a href="#twitter">Twitter</a>
+                </p>
         </footer>
 
     )
